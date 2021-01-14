83879322f7176617e10c633f7b5b0b46b0389ac1
Rory <rorygp@gmail.com>
2021-01-14 09:35:14 +0200

    sagas included in the project

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,6 +1,6 @@
 import { createSelector } from 'reselect';
 
-const selectShop = state => state.shop;
+const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
   [selectShop],
@@ -9,11 +9,21 @@ export const selectCollections = createSelector(
 
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
-)
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
+);
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector(
-    [selectCollections],
-    (collections) => (collections ? collections[collectionUrlParam] : null)
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
   );
+
+export const selectIsCollectionFetching = createSelector(
+  [selectShop],
+  (shop) => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+  [selectShop],
+  (shop) => !!shop.collections
+);
